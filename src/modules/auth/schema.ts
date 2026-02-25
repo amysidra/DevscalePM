d20bca08291860d5038adbd29272b8ace66b859b
amysidra <amysidra@gmail.com>
2026-02-25 17:53:07 +0700

    commit

--- src/modules/auth/schema.ts
+++ src/modules/auth/schema.ts
@@ -1,12 +1,12 @@
 import z from "zod";
 
 export const registerSchema = z.object({
-  username: z.string().min(3, "username minimal 3 karakter"),
-    email: z.email("format email tidak benar"),
-  password: z.string().min(8, "password minimal 8 karakter"),
+	username: z.string().min(3, "username minimal 3 karakter"),
+	email: z.email("format email tidak benar"),
+	password: z.string().min(8, "password minimal 8 karakter"),
 });
 
 export const loginSchema = z.object({
-  email: z.email("format email tidak benar"),
-  password: z.string().min(8, "password minimal 8 karakter"),
+	email: z.email("format email tidak benar"),
+	password: z.string().min(8, "password minimal 8 karakter"),
 });
--- src/modules/projects/router.ts
+++ src/modules/projects/router.ts
@@ -4,38 +4,40 @@ import { prisma } from "../../utils/prisma";
 
 export const projectsRouter = new Hono()
 
-  .use("*", authMiddleware)
+	.use("*", authMiddleware)
 
-  .get("/", async (c) => {
+	.get("/", async (c) => {
+		const payload = c.get("jwtPayload");
+		const userId = payload.sub;
 
-    const projectsList = await prisma.project.findMany({
-      where: {
-        userId,
-      },
-    });
-    return c.json({ message: "List of projects" }, 200);
-  })
+		const projectsList = await prisma.project.findMany({
+			where: {
+				userId,
+			},
+		});
+		return c.json({ message: "List of projects", data: projectsList }, 200);
+	})
 
-  .get("/:id", async (c) => {
-    const { id } = c.req.param();
-    return c.json({ message: `Project details for ID: ${id}` }, 200);
-  })
+	.get("/:id", async (c) => {
+		const { id } = c.req.param();
+		return c.json({ message: `Project details for ID: ${id}` }, 200);
+	})
 
-  .post("/", async (c) => {
-    const body = await c.req.json();
-    return c.json({ message: "Project created", data: body }, 201);
-  })
+	.post("/", async (c) => {
+		const body = await c.req.json();
+		return c.json({ message: "Project created", data: body }, 201);
+	})
 
-  .put("/:id", async (c) => {
-    const { id } = c.req.param();
-    const body = await c.req.json();
-    return c.json(
-      { message: `Project with ID: ${id} updated`, data: body },
-      200,
-    );
-  })
+	.put("/:id", async (c) => {
+		const { id } = c.req.param();
+		const body = await c.req.json();
+		return c.json(
+			{ message: `Project with ID: ${id} updated`, data: body },
+			200,
+		);
+	})
 
-  .delete("/:id", async (c) => {
-    const { id } = c.req.param();
-    return c.json({ message: `Project with ID: ${id} deleted` }, 200);
-  });
+	.delete("/:id", async (c) => {
+		const { id } = c.req.param();
+		return c.json({ message: `Project with ID: ${id} deleted` }, 200);
+	});
